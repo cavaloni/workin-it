a4c2d3765c6b759b12e5d6b63b1d39a44f0676b1
cavaloni <chad.avalon1@gmail.com>
2017-04-18 05:04:23 -0700

    updated home message

--- js/components/home/home.jsx
+++ js/components/home/home.jsx
@@ -97,6 +97,8 @@ class Home extends Component {
                         }
                         >
                     Hello {this.props.profileData.user}!
+                    <p>Apologies for any delays in usage. You may require to logout and back in to work properly.
+                    Still working out bugs on the way to version 1.0. Thanks for using!</p>
                     </ListItem>
                         {newFriends}
                     </List>
